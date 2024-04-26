5589287cd2c1d819204efe62639c27da14b5a653
Lebene Gbebleou-Sleem <128799752+LebeneSleem@users.noreply.github.com>
2024-04-26 23:22:57 +0000

    Updated to handle assignments

--- models/task.js
+++ models/task.js
@@ -1,5 +1,13 @@
+const mongoose = require('mongoose');
+
 const taskSchema = new mongoose.Schema({
+  name: {type: String, required: true },
   title: { type: String, required: true },
   description: String,
   status: { type: String, enum: ['pending', 'in-progress', 'completed'], default: 'pending' },
   assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
