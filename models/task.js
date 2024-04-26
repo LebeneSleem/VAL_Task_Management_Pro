c1089ed8b3fac35c3c3106a593cf3a383ad42c5d
Lebene Gbebleou-Sleem <128799752+LebeneSleem@users.noreply.github.com>
2024-04-26 23:30:15 +0000

    Added integration with version control:

--- models/task.js
+++ models/task.js
@@ -6,7 +6,9 @@ const taskSchema = new mongoose.Schema({
   description: String,
   status: { type: String, enum: ['To Do', 'in-progress', 'pending review' 'completed'], default: 'To Do' },
   assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-
+  repository: { type: String, required: true },
+  branch: { type: String, required: true },
+  commitId: { type: String, required: true },
 
 const Task = mongoose.model('Task', taskSchema);
 
