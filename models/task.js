8c439b3b378c33cadb6340f912a24152f0a30b21
Lebene Gbebleou-Sleem <128799752+LebeneSleem@users.noreply.github.com>
2024-04-30 21:47:46 +0000

    Updated

--- models/task.js
+++ models/task.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-  name: {type: String, required: true },
+  name: { type: String, required: true },
   title: { type: String, required: true },
   description: String,
-  status: { type: String, enum: ['To Do', 'in-progress', 'pending review' 'completed'], default: 'To Do' },
+  status: { type: String, enum: ['To Do', 'In Progress', 'Pending Review', 'Completed'], default: 'To Do' },
   assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   repository: { type: String, required: true },
   branch: { type: String, required: true },
-  commitId: { type: String, required: true },
+  commitId: { type: String, required: true }
+});
 
 const Task = mongoose.model('Task', taskSchema);
 
