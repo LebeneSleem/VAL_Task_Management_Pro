78170d3009023b3cca6f582676f19078d28d8586
Lebene Gbebleou-Sleem <128799752+LebeneSleem@users.noreply.github.com>
2024-04-30 19:53:41 +0000

    For User Page

---
+++ user.js
@@ -0,0 +1,6 @@
+document.getElementById('logout-btn').addEventListener('click', logout);
+
+function logout() {
+  // Redirect to logout endpoint or clear session/cookie
+  window.location.href = 'index.html';
+}
